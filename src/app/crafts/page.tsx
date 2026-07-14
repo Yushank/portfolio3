7abde51c184daf8b4840eead0ac2d2a73f61f527
Yushank <yushank2001@gmail.com>
2026-07-14 14:04:47 +0530

    Added all crafts page

--- src/app/crafts/page.tsx
+++ src/app/crafts/page.tsx
@@ -1,12 +1,16 @@
+import Container from "@/components/container";
+import { DottedSeparator } from "@/components/separator";
 import Link from "next/link";
-import React from "react";
 
-export const Crafts = () => {
+export default function CraftsPage() {
   return (
     <div>
       <div className="px-4">
-        <div className="text-md leading-snug text-subdued">Crafts</div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mt-4 max-w-full mx-auto">
+        <Container className="flex-col items-center mt-10 mb-10">
+          <p className="text-md text-subdued py-4">Some code snippets</p>
+          <DottedSeparator />
+        </Container>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2 mt-4 max-w-full">
           <CraftCard
             url={""}
             webm={
@@ -44,18 +48,10 @@ export const Crafts = () => {
             }
           />
         </div>
-        <div className="flex items-center justify-end mt-4">
-          <Link
-            className="text-md text-subdued hover:text-default cursor-pointer"
-            href={"/crafts"}
-          >
-            See more
-          </Link>
-        </div>
       </div>
     </div>
   );
-};
+}
 
 type craftCardProp = {
   url: string;
@@ -66,8 +62,8 @@ type craftCardProp = {
 const CraftCard = ({ url, webm, mp4 }: craftCardProp) => {
   return (
     <Link
-      href={url}
       className="group relative block rounded-xl aspect-1760/1100 bg-secondary/10 border border-subdued/20 cursor-default overflow-hidden"
+      href={url}
     >
       <img
         aria-hidden="true"
@@ -75,13 +71,13 @@ const CraftCard = ({ url, webm, mp4 }: craftCardProp) => {
         src={"public\images\blur.png"}
         className="absolute inset-0 h-full w-full object-cover rounded-[10px]"
         style={{
+          transform: "translateZ(0)",
           filter: "blur(32px)",
           scale: "(1.1)",
-          transform: "translateZ(0)",
         }}
       />
       <video
-        className="relative h-full w-full object-cover aspect-1760/1100 rounded-[10px] transition-transform duration-500 ease-out motion-reduce:transition-none"
+        className="relative h-full w-full object-cover rounded-[10px] aspect-1760/1100 transition-transform duration-500 ease-out motion-reduce:transition-none"
         autoPlay
         loop
         muted
@@ -91,9 +87,10 @@ const CraftCard = ({ url, webm, mp4 }: craftCardProp) => {
         <source src={webm} type="video/webm" />
         <source src={mp4} type="video/mp4" />
       </video>
-      <div className="absolute bottom-0 left-0 w-full p-1 translate-y-full opacity-0 blur-sm transition-all duration-300 ease-out group-hover:translate-y-0 group-hover:opacity-100 group-hover:blur-none">
+
+      <div className="absolute bottom-0 left-0 w-full p-1 translate-y-full opacity-0 blur-sm group-hover:translate-y-0 group-hover:opacity-100 group-hover:blur-none duration-300 ease-out transition-all">
         <div className="bg-blue-100 w-full rounded-lg py-1 border border-subdued/20 cursor-pointer flex items-center justify-center">
-          <p className="font-medium text-sm text-default">View Live</p>
+          <p className="font-medium text-sm text-default">View live</p>
         </div>
       </div>
     </Link>
